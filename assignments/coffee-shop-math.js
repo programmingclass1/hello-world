7f384d30ac7907bc149b99b0d909e1dbdc2d8bca
Aaron Hoffman <hoffman.aaron.d@gmail.com>
2016-10-13 19:26:12 +0000

    refactor coffee shop to use functions

---
+++ assignments/coffee-shop-math.js
@@ -0,0 +1,26 @@
+ 
+function calculateWeeklySales(sun, mon, tue, wed, thr, fri, sat) {
+    
+    // calculate total sales for the week
+    var totalSales = sun + mon + tue + wed + thr + fri + sat;
+    
+    // display total sales
+    console.log(totalSales);
+}
+
+
+// declare variables for each day
+var sunday = 240;
+var monday = 100;
+var tuesday = 102;
+var wednesday = 80;
+var thursday  = 120;
+var friday = 114;
+var saturday = 400;
+
+
+// call function to calculate weekly sales
+calculateWeeklySales(sunday, monday, tuesday, wednesday, thursday, friday, saturday);
+
+var a = 1;
+
